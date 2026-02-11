14ad2d12433f673a57ea0d420e83ec00bbe7d05a
Jarif Ahmed <your_github_email@gmail.com>
2026-02-11 18:00:20 +0600

    added all files codes updates

--- script/index.js
+++ script/index.js
@@ -4,9 +4,9 @@ const loadLessons = () => {
     .then((json) => displayLesson(json.data));
 };
 const loadLevelWord = (id) => {
-    const url = 'https://openapi.programming-hero.com/api/level/${id}';
+    const url = `https://openapi.programming-hero.com/api/level/${id}`;
     fetch(url)
-    .then(res => res.json())
+    .then((res) => res.json())
     .then((data) => displayLevelWord(data));
 
 };
@@ -19,14 +19,18 @@ const displayLevelWord = (words) => {
         // console.log(word);
         const card = document.createElement("div");
         card.innerHTML = `
-        <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5">
-    <h2>Eager</h2>
-    <p>Meaning/Pronounciation</p>
-    <div>"আগ্রহী / ইগার"</div>
+        <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+    <h2 class="font-bold text-2xl">${word.word}</h2>
+    <p class="font-bold">Meaning/Pronounciation</p>
+    <div class="text-2xl front-bangla">${word.meaning} / ${word.pronunciation}</div>
+    <div class="flex justify-between items-center">
+        <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+        <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+    </div>
 </div>
         `;
         wordContainer.append(card);
-    });
+    })
 };
 
 
@@ -40,7 +44,7 @@ const displayLesson = (lessons) => {
         // console.log(lesson);
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML = `
-        <button class="btn btn-outline btn-primary">
+        <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
         <i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}</button>
         `;
         //4. append into container
